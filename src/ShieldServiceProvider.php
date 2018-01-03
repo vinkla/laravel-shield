01be9c0574ff5e6c75047c443dfac0c7e5f8c5d1
Vincent Klaiber <vincentklaiber@gmail.com>
2018-01-03 10:26:23 +0100

    Fixed config in phars

--- src/ShieldServiceProvider.php
+++ src/ShieldServiceProvider.php
@@ -42,7 +42,7 @@ class ShieldServiceProvider extends ServiceProvider
      */
     protected function setupConfig()
     {
-        $source = realpath(__DIR__.'/../config/shield.php');
+        $source = realpath($raw = __DIR__.'/../config/shield.php') ?: $raw;
 
         if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([$source => config_path('shield.php')]);
